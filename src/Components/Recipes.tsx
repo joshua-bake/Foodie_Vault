5ff4d4f977172332049137c8a11898bce51e8882
Joshua Baker <joshua.jbaker12@icloud.com>
2024-07-18 13:40:53 -0400

    added search feature

--- src/Components/Recipes.tsx
+++ src/Components/Recipes.tsx
@@ -65,6 +65,11 @@ export default function Recipes() {
         setSearch(e.currentTarget.value)
     }
 
+    if (!recipes) {
+        return <div className="lds-hourglass"></div>
+
+    }
+
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
